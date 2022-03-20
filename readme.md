dd17ff8cd0fa089071addec47514a92176bd81a9
Ugne Mikulyte <ugn3mikulyte@gmail.com>
2022-03-20 21:07:08 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# BlogSpace
+
+This is a small project to practice API.
+
+Built with HTML, CSS and vanilla JavaScript.
+
+This blog app uses the JSON placeholder API to get fake blog entries. I have included a form that allows you to create a new blog post with a title and body that will appear above previous posts.
+
+To post, the user must fill out both the title and the body.
---
+++ script.js
@@ -0,0 +1,51 @@
+let postsArray = [];
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `<div class="post">
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            </div>
+        `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    postsArray = data.slice(0, 5);
+    renderPosts();
+  });
+
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (postTitle == "" || postBody == "") {
+    alert("Please enter Title and Body");
+    return false;
+  } else {
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+      .then((res) => res.json())
+      .then((post) => {
+        postsArray.unshift(post);
+        renderPosts();
+        document.getElementById("new-post").reset();
+      });
+  }
+});
